1288b2f2bc7d845b87b3dd3442ae9708733ed34c
Ruonan Yan <ruonanyan.newyork@gmail.com>
2026-08-07 17:37:20 -0400

    fix: hardcode supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://njvbkhhtpmsghggqmbsg.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdmJraGh0cG1zZ2hnZ3FtYnNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMjM4MzksImV4cCI6MjEwMTY5OTgzOX0.Cy12ZdMgCMOkhWZqPhNn0S2SUsFDJP5MF5bt6-arjcg'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
